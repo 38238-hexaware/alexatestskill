03cc2c40ca0a5f8a9cf2a23ae7dd7c29b5ee610e
Srinivasan Velu <ursfriendly_sri@yahoo.com>
2017-09-08 14:35:13 +0530

    First commit

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ var app=express();
 var reqnew = require('request');
 app.get("/",function(req,res){
 reqnew('http://widgets.fabulously40.com/horoscope.json?sign=capricorn', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred 
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-  console.log('body:', body); // Print the HTML for the Google homepage. 
+res.send(JSON.stringify(body));
 });
 })
 
