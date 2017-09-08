3da17896d7c6daa44c9c6b8d03da17b462d05fa9
Srinivasan Velu <ursfriendly_sri@yahoo.com>
2017-09-08 15:25:01 +0530

    new co mmd

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/", function (req, res) {
     //     .catch(function (err) {
     //         res.send(JSON.stringify(err)); 
     //     });
-res.send('srini');
+
 })
 
 // const options = {  
